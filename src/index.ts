6be9b29a3a5fa213489f2bc7b2b1fc501b46d3e9
Em <emily.clay.k@gmail.com>
2020-04-06 11:15:32 -0700

    converts to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-const debounce = (func, wait = 20) => {
-  let timeout;
-  return (...args) => {
+const debounce = (func: Function, wait = 20) => {
+  let timeout = 0;
+  return (...args: any[]) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(args), wait);
   };
 };
 
-const isOnScreen = (el, amount = 0) => {
+const isOnScreen = (el: Element, amount = 0) => {
   const { top, bottom, height } = el.getBoundingClientRect();
   const ratio = height * amount;
   return top + ratio < window.innerHeight && bottom - ratio > 0;
 };
 
-const slideImages = (images) => {
+const slideImages = (images: Element[]) => {
   images.forEach((image) => {
     if (isOnScreen(image, 0.25)) {
       image.classList.remove('hidden');
